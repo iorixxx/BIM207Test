da4260c2f99a128351e7e5b7342031a189e74fbb
Ahmet Arslan <iorixxx@yahoo.com>
2021-11-08 15:30:39 +0300

    print os information for me

--- src/main/java/edu/estu/App.java
+++ src/main/java/edu/estu/App.java
@@ -1,13 +1,8 @@
 package edu.estu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
     }
 }
